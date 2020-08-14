f32e9f9e11b180dd5e8acd39c7d82d48e0ff10f4
Lalo <eduardo.contreras@grt.mx>
2020-08-13 20:08:00 -0500

    Home y formularios funcionando - EC

--- src/contact.js
+++ src/contact.js
@@ -1,36 +1,82 @@
  contact = function() {
-    var txtName = $("#txtName").value;
-    var txtPhone = $("#txtPhone").value;
-    var txtMail = $("#txtMail").value;
-    var txtCompany = $("#txtCompany").value;
-    var txtHow = $("#txtHow").value;
-    var settings = {
-        "url": "src/contact.php",
-        "method": "POST",
-        "timeout": 0,
-        "params" : {
-            txtName:txtName,
-            txtPhone:txtPhone,
-            txtMail:txtMail,
-            txtCompany:txtCompany,
-            txtHow:txtHow
-        },
-        "dataType":"json"
-    };
-    
-    $.ajax(settings).done(function (response) {
-        if(response != ""){
-            if(response.status === "success"){
-                alert("Un profesional de nuestra área de contacto, se comunicará contigo a la brevedad");
-                $("#frmContact")[0].reset();
-            }else{
-                alert("Intenta enviar de nuevo el formulario");
-                console.log("Something wrong");
-             }
-        }else{
-            alert("Algo no va bien!, envianos un correo a contacto@grt.mx");
-            console.log("Response without data");
-        }
-    });
 
+    var valid = validateForm();
+
+    if(valid){
+               
+        var txtName = $("#txtName").val();
+        var txtPhone = $("#txtPhone").val();
+        var txtMail = $("#txtMail").val();
+        var txtCompany = $("#txtCompany").val();
+        var txtHow = $("#txtHow").val();
+    
+
+        var settings = {
+            url : "src/contact.php",
+            method : "POST",
+            timeout : 0,
+            data : {
+                txtName : txtName,
+                txtPhone : txtPhone,
+                txtMail : txtMail,
+                txtCompany : txtCompany,
+                txtHow : txtHow
+            },
+            dataType : "json"
+        };
+        
+        $.ajax(settings).done(function (response) {
+            if(response != ""){
+                if(response.status === "success"){
+                    var message = "Un profesional de nuestra área de contacto, se comunicará contigo a la brevedad";
+                    $("#divAlertMessage").html("<strong>Hola! </strong> <br>" + message);
+                    $("#divContentAlert").toggle();
+                    $("#frmContact")[0].reset();
+                    alert(message);
+                    $('#exampleModal2').modal('hide')
+
+                }else{
+                    var message = "Intenta enviar de nuevo el formulario";
+                    $("#divAlertMessage").html("<strong>Ups! </strong> <br>" + message);
+                    $("#divContentAlert").toggle();
+                    console.log("Something wrong");
+                 }
+            }else{               
+                    var message = "Algo no va bien!, envianos un correo a contacto@grt.mx";
+                    $("#divAlertMessage").html("<strong>=(</strong> <br>" + message);
+                    $("#divContentAlert").toggle();
+                console.log("Response without data");
+            }
+        });
+    }   
 };
+
+
+validateForm = function (){
+
+    console.log("Validando");
+
+    var txtName = $("#txtName").val();
+    var txtPhone = $("#txtPhone").val();
+    var txtMail = $("#txtMail").val();
+    var txtHow = $("#txtHow").val();
+    var valid = false;
+    var message = "";
+
+    message += ($.trim(txtName) == "") ? "Es necesario escribir tu nombre <br>" : "";
+    message += ($.trim(txtPhone) == "") ? "Es necesario escribir tu telefono <br>" : "";
+    message += ($.trim(txtMail) == "") ? "Es necesario escribir tu email <br>" : "";
+    message += ($.trim(txtHow) == "") ? "Es necesario escribir como te podemos ayduar <br>" : "";
+
+    if($.trim(message) != ""){
+        $("#divAlertMessage").html("<strong>Atenci&oacute;n</strong> <br>" + message);
+        $("#divContentAlert").toggle();
+        valid = false;
+    }else{
+        valid = true;
+        $("#divAlertMessage").html("<strong>Atenci&oacute;n</strong> Enviando tu información <br>");
+        $("#divContentAlert").toggle();
+    }
+
+    return valid;
+}
--- src/contact.php
+++ src/contact.php
@@ -6,23 +6,24 @@
 @$txtCompany = $_POST["txtCompany"];
 @$txtHow = $_POST["txtHow"];
 
-$url = "https://hooks.zapier.com/hooks/catch/3888697/ozebwun/?name=".$txtName."&phone=".$txtPhone."&mail=".$txtMail."&company=".$txtCompany."&how=".$txtHow;
 
 
-$curl = curl_init();
+$url = "https://hooks.zapier.com/hooks/catch/3888697/ozebwun?name=" . urlencode($txtName) . "&phone=" . urlencode($txtPhone) . "&mail=" . urlencode($txtMail) . "&company=" . urlencode($txtCompany) . "&how=" . urlencode($txtHow);
 
-curl_setopt_array($curl, array(
-  CURLOPT_URL => $url,
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "POST",
-));
+ $curl = curl_init();
 
-$response = curl_exec($curl);
-
-curl_close($curl);
-echo $response;
+ curl_setopt_array($curl, array(
+   CURLOPT_URL => $url,
+   CURLOPT_RETURNTRANSFER => true,
+   CURLOPT_ENCODING => "",
+   CURLOPT_MAXREDIRS => 10,
+   CURLOPT_TIMEOUT => 0,
+   CURLOPT_FOLLOWLOCATION => true,
+   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+   CURLOPT_CUSTOMREQUEST => "POST",
+ ));
+ 
+ $response = curl_exec($curl);
+ 
+ curl_close($curl);
+ echo $response;
